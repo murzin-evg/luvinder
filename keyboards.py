9a9b278086b9852125f9e5d8dfece3a802a0ea19
murzin-evg <123813975+murzin-evg@users.noreply.github.com>
2023-04-27 12:01:56 +0300

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,77 @@
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+def start_keyboard():
+    keyboard = VkKeyboard()
+
+    keyboard.add_button(
+        label='Начать поиск',
+        color=VkKeyboardColor.PRIMARY
+    )
+
+    keyboard.add_line()
+    
+    keyboard.add_button(
+        label='Инструкция',
+        color=VkKeyboardColor.SECONDARY
+    )
+    
+    return keyboard.get_keyboard()
+
+def main_keyboard():
+    keyboard = VkKeyboard()
+
+    keyboard.add_button(
+        label='Начать поиск',
+        color=VkKeyboardColor.SECONDARY
+    )
+
+    keyboard.add_button(
+        label='Дальше',
+        color=VkKeyboardColor.PRIMARY
+    )
+
+    keyboard.add_line()
+    
+    keyboard.add_button(
+        label='Добавить в Избранные',
+        color=VkKeyboardColor.POSITIVE
+    )
+
+    keyboard.add_button(
+        label='Добавить в Черный список',
+        color=VkKeyboardColor.NEGATIVE
+    )
+
+    keyboard.add_line()
+
+    
+    keyboard.add_button(
+        label='Избранные',
+        color=VkKeyboardColor.SECONDARY
+    )
+
+    keyboard.add_button(
+        label='Черный список',
+        color=VkKeyboardColor.SECONDARY
+    )
+
+    keyboard.add_line()
+
+    keyboard.add_button(
+        label='Удалить из Избранных',
+        color=VkKeyboardColor.PRIMARY
+    )
+
+    keyboard.add_button(
+        label='Удалить из Черного списка',
+        color=VkKeyboardColor.PRIMARY
+    )
+
+    keyboard.add_line()
+    
+    keyboard.add_button(
+        label='Инструкция',
+        color=VkKeyboardColor.SECONDARY
+    )
+
+    return keyboard.get_keyboard()
